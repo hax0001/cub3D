b16dce22803d8262cf822775802082d52bd5aae4
Aymane Kajjou <akajjou@c3r6p10.1337.ma>
2025-01-11 22:40:34 +0100

    parse

--- main.c
+++ main.c
@@ -6,40 +6,12 @@
 /*   By: akajjou <akajjou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 14:23:38 by nait-bou          #+#    #+#             */
-/*   Updated: 2025/01/11 21:30:27 by akajjou          ###   ########.fr       */
+/*   Updated: 2025/01/11 22:40:07 by akajjou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Includes/cub3d.h"
 
-
-bool    FileName_check( int argc, char **argv)
-{
-    int     FileLen;
-    char    *FileName;
-    
-    if (argc != 2)
-    {
-        printf("Error\nUsage : ./cub3d <map>\n");
-        return false;
-    }
-    FileLen = strlen(argv[1]) - 4;
-    FileName = argv[1];
-    if (strcmp( FileName + FileLen, ".cub") != 0)
-    {   
-        printf("Error\nBad extension\n");
-        return false;
-    }   
-    return true;
-}
-
-bool    parse_map( void )
-{
-    t_global *global;
-
-    global = *get_heap();
-    global->data->map    
-}
 
 
 int main(int ac, char **av)
@@ -51,7 +23,7 @@ int main(int ac, char **av)
     global = (t_global *)malloc(sizeof(t_global)); // this memory shoud freed manually with free function
     ft_memset(global, 0, sizeof(t_global));
     *get_heap() = global;
-    if (parse_map() == false)
+    if (parses_map(av[1]) == false)
         return (2);
     // cub3d();
     
--- parse/parse_map.c
+++ parse/parse_map.c
@@ -6,13 +6,72 @@
 /*   By: akajjou <akajjou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 18:26:26 by nait-bou          #+#    #+#             */
-/*   Updated: 2025/01/11 21:22:30 by akajjou          ###   ########.fr       */
+/*   Updated: 2025/01/11 22:39:38 by akajjou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/cub3d.h"
 
-t_data  *parse_map()
+bool        write_map(char ***maps, char *FileName) // this fct read the file only and return it in a double array
 {
-    char **map
+	char	*str1;
+	int		fd;
+	char	*str2;
+	char	*tmp;
+
+	str2 = 0;
+	fd = open(FileName, O_RDWR);
+	str1 = get_next_line(fd);
+    if (fd == -1 && str1 == NULL)
+    {
+        printf("Error\nFatal Error While Reading The File\n");
+        return false;
+    }
+	while (str1)
+	{
+		tmp = ft_strjoin(str2, str1);
+		free(str2);
+		str2 = tmp;
+		free(str1);
+		str1 = get_next_line(fd);
+	}
+    close(fd);
+    *maps = ft_split(str2, '\n');
+    return (free(str2), true);
+}
+
+bool    map_checker(char **map) // this fct will ... mzl ma3rft
+{
+    return (true);
+}
+
+bool    parses_map(char *FileName) // this fct will check if we have a valid map
+{
+    char **map;
+    
+    if (write_map(&map, FileName) == false)
+        return (false);
+    if (map_check(map) == false)
+        return (false);
+
+    return (true);
+}
+bool    FileName_check( int argc, char **argv) // this fct check only the FileName extesion 
+{
+    int     FileLen;
+    char    *FileName;
+    
+    if (argc != 2)
+    {
+        printf("Error\nUsage : ./cub3d <map>\n");
+        return (false);
+    }
+    FileLen = strlen(argv[1]) - 4;
+    FileName = argv[1];
+    if (strcmp( FileName + FileLen, ".cub") != 0)
+    {   
+        printf("Error\nBad extension\n");
+        return (false);
+    }   
+    return (true);
 }
